d2eeb1b77ea8c60b0d26274f88daa9eccb1d8f0c
ALEKSANDR IUSKEVICH <107239203+ALEKSANDR24101974@users.noreply.github.com>
2023-04-04 20:32:35 +0000

    lesson№1

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log('privet');
-const grom = '78.8 volt';
+console.log("privet")
+const grom = '78.8 volt'
+let grum = parseFloat(grom)
+console.log(grum)
+let grim = '90986889vhguhk'
+console.log(parseFloat(grim))
+let grym = new Date()
+console.log(grym)
+console.log(grym.valueOf())
+grum = Number(grum)
+console.log(grum+grim)
+grum = grum.toString()
+console.log(grum)
+//glava -4 upravl ctr81
