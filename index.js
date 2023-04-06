ee77683f7599719578e283336cfa3ee84833979f
ALEKSANDR IUSKEVICH <107239203+ALEKSANDR24101974@users.noreply.github.com>
2023-04-06 20:35:03 +0000

    cikly+

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let grym = new Date()
 console.log(grym)
 console.log(grym.valueOf())
 grum = Number(grum)
-console.log(grum+grim)
+console.log(grum + grim)
 grum = grum.toString()
 console.log(grum)
 //glava -4 upravl ctr81
@@ -18,3 +18,16 @@ let f0 = Math.random();
 console.log(f0);
 //console.log(f1);
 //console.log(randomF);
+let amor = 50;
+let belamor = 1;
+while (belamor <= 10 && belamor >= 1) {
+    belamor++;
+    amor++;
+    console.log(amor, belamor)
+}
+for (belamor = 0; belamor < 10; belamor++) {
+    let matem = (Math.random() + 1) * 23;
+    if (matem > 30) console.log(matem);
+    else { console.log(matem + "-хуйня") }
+}
+
